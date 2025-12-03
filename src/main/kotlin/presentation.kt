d8d5f5dec8ba08f69575460bb537216f7f042e2a
Kwon-HyukJun <ableman06@yonsei.ac.kr>
2025-12-04 02:58:51 +0900

    add stt, presentation fun

--- src/main/kotlin/presentation.kt
+++ src/main/kotlin/presentation.kt
@@ -13,6 +13,85 @@ import io.ktor.server.routing.post
 import kotlinx.serialization.json.Json
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.route
+import io.ktor.server.request.receiveMultipart
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.PartData
+import io.ktor.http.content.streamProvider
+import io.ktor.http.HttpStatusCode
+import java.io.File
+
+private const val WHISPER_PATH =
+    "C:\\Users\\ablem\\whisper.cpp\\build\\bin\\Release\\whisper-cli.exe"
+
+private const val MODEL_PATH =
+    "C:\\Users\\ablem\\whisper.cpp\\models\\ggml-small.bin"
+
+
+fun runWhisper(audioFile: File): String {
+
+    val outputTxt = File(audioFile.absolutePath + ".txt")
+
+    val process = ProcessBuilder(
+        WHISPER_PATH,
+        "-m", MODEL_PATH,
+        "-f", audioFile.absolutePath,
+        "-l", "ko",          // 언어 강제: 한국어
+        "-otxt"
+    )
+        .redirectErrorStream(true)
+        .directory(File("C:\\Users\\ablem\\whisper.cpp\\build\\bin\\Release"))
+        .start()
+
+    val whisperOutput = process.inputStream.bufferedReader().readText()
+    val exitCode = process.waitFor()
+
+    if (exitCode != 0) {
+        throw RuntimeException("Whisper 실행 오류: $whisperOutput")
+    }
+
+    // txt 파일 읽기
+    val resultText = outputTxt.readText()
+
+    // 불필요 파일 삭제
+    outputTxt.delete()
+
+    return resultText
+}
+
+private suspend fun callGpt(topic: String, text: String): String
+{
+    val prompt = """
+                "${topic}"에 대해 발표 대본을 써봤는데 숫자로만 평가해줘. 평가 근거를 들어서.
+                독후감: 
+                 "${text}"
+            """.trimIndent()
+
+    val requestBody = LLMRequest(
+        model = "gpt-4o-mini",
+        messages = listOf(
+            ChatMessage("user", prompt)
+        )
+    )
+
+    val llmResponse: HttpResponse = client.post("https://api.openai.com/v1/chat/completions") {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        header(HttpHeaders.Authorization, "Bearer ${System.getenv("OPENAI_API_KEY")}")
+
+        setBody(requestBody)
+    }
+
+    val resultText = llmResponse.bodyAsText()
+
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val parsed = json.decodeFromString<ChatCompletionResponse>(resultText)
+
+    val content = parsed.choices.first().message.content
+
+    return content
+}
 
 fun Route.presentationRouter()
 {
@@ -20,7 +99,59 @@ fun Route.presentationRouter()
     {
         post()
         {
-            //pass
+            val multipart = call.receiveMultipart()
+            var audioFile: File? = null
+            var topic: String? = null
+
+            multipart.forEachPart { part ->
+                when (part) {
+
+                    is PartData.FormItem -> {
+                        if (part.name == "topic") {
+                            topic = part.value
+                        }
+                    }
+
+                    is PartData.FileItem -> {
+                        val ext = File(part.originalFileName!!).extension
+                        val tempFile = File.createTempFile("upload_", ".$ext")
+
+                        tempFile.outputStream().use { output ->
+                            part.streamProvider().use { input ->
+                                input.copyTo(output)
+                            }
+                        }
+                        audioFile = tempFile
+                    }
+
+                    else -> {}
+                }
+                part.dispose()
+            }
+
+            if (audioFile == null) {
+                call.respond(HttpStatusCode.BadRequest, "녹음 파일이 없습니다.")
+                return@post
+            }
+            if (topic == null) {
+                call.respond(HttpStatusCode.BadRequest, "주제 없습니다.")
+                return@post
+            }
+
+            try {
+                val result = runWhisper(audioFile!!)
+                audioFile!!.delete()
+
+                val reComent = callGpt(topic!!, result)
+
+                call.respond(reComent)
+
+            } catch (e: Exception) {
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    "STT 오류: ${e.message}"
+                )
+            }
         }
     }
 }
--- src/main/kotlin/review.kt
+++ src/main/kotlin/review.kt
@@ -9,26 +9,23 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.content.PartData
 import io.ktor.http.content.forEachPart
-import io.ktor.server.request.receive
 import io.ktor.server.request.receiveMultipart
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
-import kotlinx.serialization.json.Json
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.route
 import io.ktor.http.content.streamProvider
-import kotlinx.serialization.internal.throwMissingFieldException
+import kotlinx.serialization.json.Json
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
-import kr.dogfoot.hwplib.reader.HWPReader
 import java.io.ByteArrayInputStream
 
-fun readHwp(bytes: ByteArray): String
+private fun readHwp(bytes: ByteArray): String
 {
     return "mock"
 }
 
-fun readPdf(bytes: ByteArray): String
+private fun readPdf(bytes: ByteArray): String
 {
     val document = PDDocument.load(ByteArrayInputStream(bytes))
     val stripper = PDFTextStripper()
@@ -38,9 +35,39 @@ fun readPdf(bytes: ByteArray): String
     return text
 }
 
-fun askGpt(text: String): String
+private suspend fun callGpt(book: String, text: String): String
 {
-    return "good"
+    val prompt = """
+                "${book}"에 대해 독후감을 써봤는데 숫자로만 평가해줘. 평가 근거를 들어서.
+                독후감: 
+                 "${text}"
+            """.trimIndent()
+
+    val requestBody = LLMRequest(
+        model = "gpt-4o-mini",
+        messages = listOf(
+            ChatMessage("user", prompt)
+        )
+    )
+
+    val llmResponse: HttpResponse = client.post("https://api.openai.com/v1/chat/completions") {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+        header(HttpHeaders.Authorization, "Bearer ${System.getenv("OPENAI_API_KEY")}")
+
+        setBody(requestBody)
+    }
+
+    val resultText = llmResponse.bodyAsText()
+
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val parsed = json.decodeFromString<ChatCompletionResponse>(resultText)
+
+    val content = parsed.choices.first().message.content
+
+    return content
 }
 
 fun Route.reviewRouter()
@@ -51,31 +78,50 @@ fun Route.reviewRouter()
         {
             val multipart = call.receiveMultipart()
             var extractedText = "파일을 찾을 수 없습니다."
-            var flag : Boolean = true
+            var book : String? = null
+            var flag = true
+            var fileBytes: ByteArray? = null
+            var fileName: String? = null
+            var reText = "지원하지 않는 형식"
 
             multipart.forEachPart { part ->
-                if(part is PartData.FileItem) {
-                    val fileName = part.originalFileName ?: ""
-                    val bytes = part.streamProvider().readBytes()
-
-
-                    if(fileName.endsWith(".hwp", ignoreCase = true)) {
-                        extractedText = readHwp(bytes)
-                    } else if(fileName.endsWith(".pdf", ignoreCase = true)) {
-                        extractedText = readPdf(bytes)
-                    } else {
-                        extractedText = "지원 안함"
-                        flag = false
+                when (part) {
+                    is PartData.FormItem -> {
+                        if (part.name == "book") {
+                            book = part.value
+                        }
                     }
 
-                    if(flag)
-                    {
-                        askGpt(extractedText)
+                    is PartData.FileItem -> {
+                        fileName = part.originalFileName
+                        fileBytes = part.streamProvider().readBytes()
                     }
+
+                    else -> {}
                 }
+                part.dispose()
             }
 
-            call.respond(extractedText)
+            if(fileBytes == null || fileName == null || book == null) {
+                call.respond("파일 받기 실패")
+                return@post
+            }
+
+            if(fileName!!.endsWith(".hwp", ignoreCase = true)) {
+                extractedText = readHwp(fileBytes!!)
+            } else if(fileName!!.endsWith(".pdf", ignoreCase = true)) {
+                extractedText = readPdf(fileBytes!!)
+            } else {
+                extractedText = "지원 안함"
+                flag = false
+            }
+
+            if(flag)
+            {
+                reText = callGpt(book!!, extractedText)
+            }
+
+            call.respond(reText)
         }
     }
 }
